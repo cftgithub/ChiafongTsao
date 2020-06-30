b1693002c42ef1fbcf26a44cda2722c0c3d399ce
cftgithub <chiafongt@gmail.com>
2020-06-29 19:54:57 -0600

    Major styling update to include typewriter header effect and carousel on index.html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,33 +1,32 @@
-// Slick carousel
-$(document).ready(function(){
-    $('.your-class').slick({
-    //   setting-name: setting-value
-        dots: true,
-    });
-    
-    $('.center').slick({
-        centerMode: true,
-        centerPadding: '60px',
-        slidesToShow: 3,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 3
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 1
-                }
-            }
-        ]
-    });
-});
+const instance = new TypeIt("#myElement", {
+    speed: 170,
+    loop: true
+})
+    .type("", {delay: 1000})
+    .type("HTML", { delay: 1000 })
+    .delete(4)
+    .type("CSS", { delay: 1000 })
+    .delete(3)
+    .type("JavaScript", { delay: 1000 })
+    .delete(10)
+    .type("JQuery", { delay: 1000 })
+    .delete(6)
+    .type("Bootstrap", { delay: 1000 })
+    .delete(9)
+    .type("Express.js", { delay: 1000 })
+    .delete(10)
+    .type("React.js", { delay: 1000 })
+    .delete(8)
+    .type("Node.js", { delay: 1000 })
+    .delete(7)
+    .type("MongoDB", { delay: 1000 })
+    .delete(7)
+    .type("MySQL", { delay: 1000 })
+    .delete(5)
+    .type("Git", { delay: 1000 })
+    .go();
+
+    // const typing = new TypeIt("#projects", {
+    //     loop: true
+    //   });
+    //   typing.go();
